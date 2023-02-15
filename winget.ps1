1458f52d337e2f72caa1087d3bf247a831f3f11d
Scott Wu <58385792+coffeemilktea@users.noreply.github.com>
2023-02-15 00:17:00 -0800

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,12 +1,9 @@
-# Download the latest release of Winget from GitHub
-Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/winget-cli-windows-x86_64.zip" -OutFile "$env:TEMP\winget.zip"
+# Downloads the latest release of Winget currently available on Github Repo
+Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/archive/refs/tags/v1.5.441-preview.zip" -OutFile "$env:TEMP\winget.zip"
 
-# Extract the contents of the ZIP file to a temporary directory
 Expand-Archive -Path "$env:TEMP\winget.zip" -DestinationPath "$env:TEMP\winget"
-
-# Move the Winget executable to a directory in the system's PATH
 Move-Item -Path "$env:TEMP\winget\winget.exe" -Destination "C:\Windows\System32\"
 
-# Clean up the temporary files
+# Delete files in Temp from installation
 Remove-Item -Path "$env:TEMP\winget.zip"
 Remove-Item -Path "$env:TEMP\winget" -Recurse
