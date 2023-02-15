b8f087173fbce0b4a5ad5ae5ff9bdc7e13bd3eb0
Scott Wu <58385792+coffeemilktea@users.noreply.github.com>
2023-02-15 02:35:01 -0800

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,9 +1,7 @@
 # Downloads the latest release of Winget currently available on Github Repo
-Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/archive/refs/tags/v1.5.441-preview.zip" -OutFile "$env:TEMP\winget.zip"
+Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$env:TEMP\winget.msixbundle"
 
-Expand-Archive -Path "$env:TEMP\winget.zip" -DestinationPath "$env:TEMP\winget"
-Move-Item -Path "$env:TEMP\winget\winget.exe" -Destination "C:\Windows\System32\"
+Add-AppPackage -path "$env:TEMP\winget.msixbundle"
 
 # Delete files in Temp from installation
-Remove-Item -Path "$env:TEMP\winget.zip"
-Remove-Item -Path "$env:TEMP\winget" -Recurse
+Remove-Item -Path "$env:TEMP\winget.msixbundle"
